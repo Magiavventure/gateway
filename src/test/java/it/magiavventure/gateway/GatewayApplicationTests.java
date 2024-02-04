18d1140e2cd5688656154fc5ef6bd741c113b2ba
Marco Polverini <marco.polverini.dev@gmail.com>
2024-02-04 16:47:23 +0100

    refactor: banner

--- src/test/java/it/magiavventure/gateway/GatewayApplicationTests.java
+++ src/test/java/it/magiavventure/gateway/GatewayApplicationTests.java
@@ -1,5 +1,6 @@
 package it.magiavventure.gateway;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,9 @@ class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int expected = 1;
+		int actual = 1;
+		Assertions.assertEquals(expected, actual);
 	}
 
 }
